5421acbe4a412b3c430be8ed7996a846312e187f
Edmund Little <edlittle96@gmail.com>
2024-10-09 21:18:52 +0100

    Single post per invokation

--- weather_report/src.py
+++ weather_report/src.py
@@ -1,11 +1,63 @@
 """
 Read sensor data from BME280 and send it over local network
 """
+import os
+
+import bme280
+import smbus2
+from dotenv import load_dotenv
+import datetime
+import requests
+import logging
+
+# Set up logging
+log_file = "~/sensor_logs.log"
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+# Load sensor location from env
+load_dotenv()
+port = os.getenv("I2C_PORT")
+address = os.getenv("I2C_ADDRESS")
+bus = smbus2.SMBus(port)
+
+# Load calibration parameters from sensor
+sensor_calibration = bme280.load_calibration_params(bus, address)
 
 
 def main():
     """
-    Test function
+    Read BME and send data to a URL
     """
 
-    print("setup worked")
+    # Read time
+    read_time = datetime.utcnow()
+
+    # Read sensor
+    try: 
+        data = bme280.sample(bus, address, sensor_calibration)
+    except Exception as err:
+        logging.error(err)
+
+    # Send data
+    try:
+        response = requests.post(
+            os.getenv("URL"),
+            json={
+                "time": read_time,
+                "temperature": data.temperature,
+                "pressure": data.pressure,
+                "humidity": data.humidity,
+            },
+        )
+        response.raise_for_status()
+    except requests.HTTPError as err:
+        logging.error(err)
+
+    # If success, log 
+    logging.info(f"Data read and sent successfully")
+
