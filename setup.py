ae1dc99e720afb1433619f07de10e9da7753fb6c
Edmund Little <edlittle96@gmail.com>
2024-10-09 22:19:25 +0100

    Update some dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
+# Read requirements from requirements.txt
+# Sorry abt the strict requirements (not)
+with open('requirements.txt', 'r') as file:
+    requirements = [line.strip() for line in file if line and not line.startswith('#')]
+
+
 setup(
     name="weather_report",
     version="0.0.0",
     py_modules=["weather_report"],  # Your main script file without the .py extension
-    install_requires=[],  # Add any dependencies here if needed
+    install_requires=requirements,  # Add any dependencies here if needed
     entry_points={
         "console_scripts": [
             "weather_report=weather_report:main",  # Define the CLI command and entry point
