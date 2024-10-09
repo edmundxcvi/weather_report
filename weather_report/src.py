435d1a4db0a8f1670545dda37b9290d4563e736d
Edmund Little <edlittle96@gmail.com>
2024-10-09 22:47:06 +0100

    Change log loc

--- weather_report/src.py
+++ weather_report/src.py
@@ -1,6 +1,7 @@
 """
 Read sensor data from BME280 and send it over local network
 """
+
 import os
 
 import bme280
@@ -9,14 +10,15 @@ from dotenv import load_dotenv
 import datetime
 import requests
 import logging
+from pathlib import Path
 
 # Set up logging
-log_file = "~/sensor_logs.log"
+log_file = Path.home() / "sensor_logs.log"
 logging.basicConfig(
     filename=log_file,
     level=logging.INFO,
     format="%(asctime)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 # Load sensor location from env
@@ -38,7 +40,7 @@ def main():
     read_time = datetime.utcnow()
 
     # Read sensor
-    try: 
+    try:
         data = bme280.sample(bus, address, sensor_calibration)
     except Exception as err:
         logging.error(err)
@@ -58,6 +60,5 @@ def main():
     except requests.HTTPError as err:
         logging.error(err)
 
-    # If success, log 
+    # If success, log
     logging.info(f"Data read and sent successfully")
-
