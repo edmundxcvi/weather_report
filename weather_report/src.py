98189be198d775d204388e950a1b7dd555629e16
Edmund Little <edlittle96@gmail.com>
2024-11-04 23:40:58 +0000

    Add different reporting lol

--- weather_report/src.py
+++ weather_report/src.py
@@ -60,6 +60,7 @@ def main():
         timeout=POST_TIMEOUT,
     )
     if response.status_code != 200:
+        print(response.status_code)
         logging.error(
             logging.error(
                 "Sensor read successfully but data post failed with error: %s",
