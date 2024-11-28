228f672d828de90f73b9ee837e3e083ef685c370
Edmund Little <edlittle96@gmail.com>
2024-11-28 22:38:33 +0000

    Stop expanding the ****ing asterisks

--- scripts/schedule_weather_report.sh
+++ scripts/schedule_weather_report.sh
@@ -17,23 +17,37 @@ fi
 
 # Create job
 ROOT_DIR="$(readlink -f $(dirname $(dirname "$BASH_SOURCE[0]}")))"
-CRON_JOB="*/$INTERVAL * * * * $ROOT_DIR/scripts/invoke_weather_report.sh"
+CRON_CMD="$ROOT_DIR/scripts/invoke_weather_report.sh"
+CRON_JOB="*/$INTERVAL * * * * $CRON_CMD"
 
 # Ensure that job is executable
 chmod +x $ROOT_DIR/scripts/invoke_weather_report.sh
 
 # Copy existing cronjobs to variable (excluding errors, which are sent to /dev/null)
-# and use grep to filter out any lines which match the job text
+CRONTAB=$(crontab -l 2>/dev/null )
+
+# Use grep to filter out any lines which match the job text
 # (-F means match strings (instead of default regex matching))
 # (-v inverts selection to select *non*-matching rows)
-CRONTAB=$(crontab -l 2>/dev/null | grep -vF $CRON_JOB)
+# Make sure to echo the variable in quotes to avoid expanding the asterisks
+# i.e. `echo "$VAR"` not `echo $VAR`
+NEW_CRONTAB=$(echo "$CRONTAB" | grep -vF $CRON_CMD)
 
 # Add new cronjob to end of crontab
-NEW_CRONTAB=$(echo -e "$CRONTAB\n$CRON_JOB")
+NEW_CRONTAB=$({
+    echo "$NEW_CRONTAB"
+    echo "$CRON_JOB"
+})
 
 # Pipe output to crontab
 # (-e interprets escaped characters so \n is a new line, not the characters \n)
-echo -e NEW_CRONTAB | crontab -
+echo "$NEW_CRONTAB" | crontab -
+
+# Check the exit code of the above statement
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to update the crontab."
+    exit 1
+fi
 
 # Report
 echo "Cron job $CRON_JOB created successfully"
