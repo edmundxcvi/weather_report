d38e784d29573fe49a2cb4c7bda94aaacac987c9
Edmund Little <edlittle96@gmail.com>
2024-12-06 21:32:07 +0000

    Add buffer

--- weather_report/src.py
+++ weather_report/src.py
@@ -2,10 +2,10 @@
 Read sensor data from BME280 and send it over local network
 """
 
-import logging
 import os
 from datetime import datetime, timezone
 from pathlib import Path
+import json
 
 import bme280
 import requests
@@ -31,6 +31,8 @@ sensor_calibration = bme280.load_calibration_params(bus, address)
 # Set post timeout
 POST_TIMEOUT = 5  # seconds
 
+# Prepare for failure
+post_buffer_path = Path(os.getenv('POST_BUFFER_PATH'))
 
 def main():
     """
@@ -48,30 +50,41 @@ def main():
     else:
         logger.debug("Sensor read successfully")
 
-    # Send data
-    try:
-        response = requests.post(
-            os.getenv("POST_URL"),
-            json={
+    # Collate into dict
+    data_dict = {
                 "time": read_time.isoformat(),
                 "temperature": data.temperature,
                 "pressure": data.pressure,
                 "humidity": data.humidity,
-            },
+            }
+
+    # Send data
+    try:
+        response = requests.post(
+            os.getenv("POST_URL"),
+            json=data_dict,
             headers={"Authorization": os.getenv("API_KEY")},
             timeout=POST_TIMEOUT,
             verify=os.getenv("SSL_CERT_PATH"),
         )
         response.raise_for_status()
+    # If data coun't be sent try to save it
+    # (This would be triggered by e.g. server unavailable)
     except requests.HTTPError as e:
         logger.error("Failed to send post request: %s", e)
+        try:
+            json.dump(data_dict, post_buffer_path / f"read_{read_time.strftime('%Y%m%d%H%M%S')}.json")
+        except OSError:
+            logger.error(f"Post request could not be saved: {e}")
+        else:
+            logger.warning(f"Post request saved to buffer.")
     else:
         logger.debug("Post request sent")
 
+    # If unexpected error code returned but post request successful then warn
     if response.status_code != 201:
-        logger.error(
-            "Sensor read successfully but data post failed with error: %s",
-            response.reason,
+        logger.warning(
+            f"Sensor read received unexpected status code {response.status_code}: {response.reason}"
         )
     else:
         logger.info("Data read and sent successfully")
